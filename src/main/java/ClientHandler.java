5a20d12c1b8b7ec08bc428846b368b3ad702e823
anas-git-author <anasgitauthor@xyz.com>
2024-02-27 20:17:36 +0530

    master replid and offset added

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -97,10 +97,11 @@ public class ClientHandler implements Runnable {
                             HashMap<String, String> infoMap = new HashMap<>();
                             System.out.println(serverDetails.getType() + " : serverdetails");
                             if (serverDetails.getType().equalsIgnoreCase("master")) {
-                                infoMap.put("role", "master");
+
                                 infoMap.put("master_replid", serverDetails.getReplid());
 //                                Integer offset = serverDetails.getOffset();
                                 infoMap.put("master_repl_offset", String.valueOf(serverDetails.getOffset()));
+                                infoMap.put("role", "master");
                             } else
                                 infoMap.put("role", "slave");
 
