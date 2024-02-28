7972e794bfec2a06aaaf16491263cab6b70ef918
anas-git-author <anasgitauthor@xyz.com>
2024-02-28 16:35:45 +0530

    added response to psync + empty RDB file

--- src/main/java/utils/Printer.java
+++ src/main/java/utils/Printer.java
@@ -93,7 +93,11 @@ public class Printer {
         String emptyRDBbase64="524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
         byte[] byteArr=new byte[emptyRDBbase64.length()/2];
         for(int i=0;i<emptyRDBbase64.length();i+=2){
-            byteArr[i/2]=String.valueOf(Integer.parseInt(emptyRDBbase64.substring(i,i+2),2)).getBytes()[0];
+            Integer a=Integer.parseInt(emptyRDBbase64.substring(i,i+1),2);
+            Integer b=Integer.parseInt(emptyRDBbase64.substring(i+1,i+2),2);
+            String x=String.valueOf(a)+String.valueOf(b);
+            System.out.println(x+":x");
+            byteArr[i/2]=x.getBytes()[0];
         }
 
         String x=byteArr.toString();
