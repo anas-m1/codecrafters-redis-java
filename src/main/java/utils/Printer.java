db11aa8955266065d2eafa75084775af9bccaa25
anas-git-author <anasgitauthor@xyz.com>
2024-02-28 16:31:42 +0530

    added response to psync + empty RDB file

--- src/main/java/utils/Printer.java
+++ src/main/java/utils/Printer.java
@@ -91,12 +91,13 @@ public class Printer {
         outputStream.flush();
 
         String emptyRDBbase64="524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
-        List<Byte> byteList=new ArrayList<>();
         byte[] byteArr=new byte[emptyRDBbase64.length()/2];
         for(int i=0;i<emptyRDBbase64.length();i+=2){
-            byteArr[i/2]=emptyRDBbase64.substring(i,i+2).getBytes()[0];
+            byteArr[i/2]=String.valueOf(Integer.parseInt(emptyRDBbase64.substring(i,i+2),2)).getBytes()[0];
         }
-        outputStream.write(byteArr);
+
+        String x=byteArr.toString();
+        outputStream.write(("$"+x.length()+clrf+x).getBytes());
         outputStream.flush();
 
     }
