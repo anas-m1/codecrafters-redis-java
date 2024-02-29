489e024284a2a2e6015389eeb4feb65c11d2a927
anas-git-author <anasgitauthor@xyz.com>
2024-02-29 12:55:20 +0530

    master sending queue of set commands to slaves

--- src/main/java/utils/Printer.java
+++ src/main/java/utils/Printer.java
@@ -90,7 +90,7 @@ public class Printer {
 
         String base64str="UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
         byte[] rdbBytes=Base64.getDecoder().decode(base64str);
-        int lenrdbBytesStr=rdbBytes.length *2;
+        int lenrdbBytesStr=rdbBytes.toString().length();
 
         outputStream.write(("$"+lenrdbBytesStr+clrf).getBytes());
         outputStream.write(rdbBytes);
